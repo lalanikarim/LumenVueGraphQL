6b8bbfef1b0af5de8f94cae72632c1e15be367fb
Karim Lalani <jimmy00784@gmail.com>
2021-03-10 08:48:00 -0600

    enabled facades and index blade template to the Lumen project

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,2 +1,11 @@
-<?php
-
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lumen Project</title>
+  </head>
+  <body>
+    <div>{{ app()->version() }}</div>
+  </body>
+</html>
