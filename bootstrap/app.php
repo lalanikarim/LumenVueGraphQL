af244d41f83cbb8aab7fcb87b5913ce11cd21649
Karim Lalani <jimmy00784@gmail.com>
2021-03-10 22:37:32 -0600

    added tinker to lumen project

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -61,6 +61,7 @@ $app->singleton(
 
 $app->configure('app');
 
+$app->configure('tinker');
 /*
 |--------------------------------------------------------------------------
 | Register Middleware
@@ -94,7 +95,7 @@ $app->configure('app');
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
-
+$app->register(\Laravel\Tinker\TinkerServiceProvider::class);
 /*
 |--------------------------------------------------------------------------
 | Load The Application Routes
